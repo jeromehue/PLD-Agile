29fd7fcdcd2ff423cf52169d5c4699312b94d813
jerome <hue.jerome@yahoo.fr>
2020-11-15 21:09:54 +0100

    Added a simple test + refactoring

---
+++ src/test/java/agile/BasicTest.java
@@ -0,0 +1,14 @@
+package agile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BasicTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
