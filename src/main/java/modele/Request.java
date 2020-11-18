d146be5710dfbb88f29f9de8d850d892de587aa9
jerome <hue.jerome@yahoo.fr>
2020-11-18 10:11:55 +0100

    Added duration of pickups and delivery in Request clas

--- src/main/java/modele/Request.java
+++ src/main/java/modele/Request.java
@@ -5,7 +5,9 @@ mport java.util.ArrayList;
 public class Request {
 	private ArrayList<Intersection> pickUpLocations;
 	private ArrayList<Intersection> deliveryLocations;
-	
+    private ArrayList<Integer> pickUpTime;
+    private ArrayList<Integer> deliveryTime;
+
 	public Request(ArrayList<Intersection> pickUps, ArrayList<Intersection> deliveries) {
 		pickUpLocations = pickUps;
 		deliveryLocations = deliveries;
