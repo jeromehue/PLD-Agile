7b9250799c311e6f7b4e3261d76b8fbad08ea241
jerome <hue.jerome@yahoo.fr>
2020-11-23 09:31:25 +0100

    Ajout de la class controller et d'un main + button listener

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,13 @@
+package controller;
+
+import modele.CityMap;
+import view.Window;
+
+public class Controller {
+
+	private Window window;
+	
+	public Controller() {
+		window = new Window();
+	}
+}
