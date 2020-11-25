18e552529cc3cefd6eb9a17490d9834d84dcc2b1
Quentin Regaud <quentin.regaud31@gmail.com>
2020-11-25 08:45:12 +0100

    Test tsp1

--- src/main/java/algo/TSP1.java
+++ src/main/java/algo/TSP1.java
@@ -2,10 +2,34 @@ package algo;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.PriorityQueue;
 
 public class TSP1 extends TemplateTSP {
+	private PriorityQueue<Double> minimumQueue;
+	
+	/*
+	protected void init () {
+		minimumQueue = new PriorityQueue<Double>();
+		int i=0;
+		int j=0;
+		for(i=0; i<g.getNbVertices(); i++) {
+			for(j=0; j<g.getNbVertices(); j++) {
+				if(i!=j) {
+					minimumQueue.add( g.getCost(i, i));
+				}
+			}
+		}
+		System.out.println("minimum = "+minimumQueue.peek());
+	}*/
+	
 	@Override
 	protected int bound(Integer currentVertex, Collection<Integer> unvisited) {
+		PriorityQueue<Double> minimumQueue = new PriorityQueue<>();
+		for(Integer index : unvisited) {
+			minimumQueue.add( g.getCost(currentVertex, index));
+		}
+		
+		//return (int)((unvisited.size()+1) * minimumQueue.peek());
 		return 0;
 	}
 
