2e461b2410e9d396eb586ba2322832769b6bcc5c
Quentin Regaud <quentin.regaud31@gmail.com>
2020-11-25 08:56:22 +0100

    pull

--- src/main/java/algo/TSP1.java
+++ src/main/java/algo/TSP1.java
@@ -5,22 +5,26 @@ import java.util.Iterator;
 import java.util.PriorityQueue;
 
 public class TSP1 extends TemplateTSP {
-	private PriorityQueue<Double> minimumQueue;
+	private Double [] minimumCost;
 	
-	/*
 	protected void init () {
-		minimumQueue = new PriorityQueue<Double>();
+		minimumCost = new Double [g.getNbVertices()];
+		PriorityQueue<Double> vertexMinimum;
 		int i=0;
 		int j=0;
 		for(i=0; i<g.getNbVertices(); i++) {
+			vertexMinimum = new PriorityQueue<>();
 			for(j=0; j<g.getNbVertices(); j++) {
 				if(i!=j) {
-					minimumQueue.add( g.getCost(i, i));
+					vertexMinimum.add( g.getCost(i, i));
 				}
 			}
+			minimumCost[i] = vertexMinimum.peek();
 		}
-		System.out.println("minimum = "+minimumQueue.peek());
-	}*/
+		for (i=0; i< g.getNbVertices(); i++) { 
+			System.out.println("minimum depuis le sommet "+i+" est "+minimumCost[i]);
+		}
+	}
 	
 	@Override
 	protected int bound(Integer currentVertex, Collection<Integer> unvisited) {
@@ -29,8 +33,8 @@ public class TSP1 extends TemplateTSP {
 			minimumQueue.add( g.getCost(currentVertex, index));
 		}
 		
-		//return (int)((unvisited.size()+1) * minimumQueue.peek());
-		return 0;
+		return (int)((unvisited.size()+1) * minimumQueue.peek());
+		//return 0;
 	}
 
 	@Override
