cb4ee681fcf19e8f104916888eab9219d0c71906
YohanMeyer <yohan.meyer@outlook.fr>
2020-12-02 08:08:59 +0100

    modifs de testTSP

--- src/test/java/algoTest/testTSP.java
+++ src/test/java/algoTest/testTSP.java
@@ -46,43 +46,12 @@ public class testTSP {
 		
 		long startTime = System.currentTimeMillis();
 		tsp.searchSolution(40000);
-		System.out.print("Solution of cost "+tsp.getSolutionCost()+" found in "
+		System.out.print("Solution of cost "+(int)tsp.getSolutionCost()+"m found in "
 				+(System.currentTimeMillis() - startTime)+"ms : ");
 		for (int i=0; i<graph.getNbVertices(); i++)
 			System.out.print(tsp.getSolution(i)+" ");
 		System.out.println("0");
 		
 		assertTrue(tsp != null);
-		/*
-		ArrayList<Segment> lseg = new ArrayList<Segment>();
-		Intersection inter1 = new Intersection(new Long(1), 0.0, 0.0, lseg);
-		Intersection inter2 = new Intersection(new Long(2), 0.0, 0.0, lseg);
-		Intersection inter3 = new Intersection(new Long(3), 0.0, 0.0, lseg);
-		Intersection inter4 = new Intersection(new Long(4), 0.0, 0.0, lseg);
-		Intersection inter5 = new Intersection(new Long(5), 0.0, 0.0, lseg);
-		Intersection inter6 = new Intersection(new Long(6), 0.0, 0.0, lseg);
-		List<Intersection> list = new ArrayList<>();
-		list.add(inter1);
-		list.add(inter2);
-		list.add(inter3);
-		list.add(inter4);
-		list.add(inter5);
-		list.add(inter6);
-		for (int nbVertices = 8; nbVertices <= 16; nbVertices += 2){
-			Intersection inter7 = new Intersection(new Long(nbVertices-1), 0.0, 0.0, lseg);
-			Intersection inter8 = new Intersection(new Long(nbVertices), 0.0, 0.0, lseg);
-			list.add(inter7);
-			list.add(inter8);
-			
-			System.out.println("Graphs with "+nbVertices+" vertices:");
-			Graph g = new CompleteGraph(list);
-			long startTime = System.currentTimeMillis();
-			tsp.searchSolution(20000, g);
-			System.out.print("Solution of cost "+tsp.getSolutionCost()+" found in "
-					+(System.currentTimeMillis() - startTime)+"ms : ");
-			for (int i=0; i<nbVertices; i++)
-				System.out.print(tsp.getSolution(i)+" ");
-			System.out.println("0");
-		}*/
 	}
 }
