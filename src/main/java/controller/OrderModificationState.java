8277bf1e8438d0e2114f41a441cfea011191d5aa
jerome <hue.jerome@yahoo.fr>
2020-12-02 12:02:40 +0100

    Fetching arguments for order modification

--- src/main/java/controller/OrderModificationState.java
+++ src/main/java/controller/OrderModificationState.java
@@ -1,6 +1,8 @@
 package controller;
 
 import modele.Way;
+import modele.Intersection;
+import modele.Tour;
 import view.Window;
 
 public class OrderModificationState implements State {
@@ -9,5 +11,12 @@ public class OrderModificationState implements State {
 	public void clickOnStep(Window w, Way wa) {
 		System.out.println("[Order Modification State] Modify order of tour in controller");
 		System.out.println("[Order Modification State] Intersection to be modify" + wa.getDeparture());
+		
+		// Testing purposes
+		int newIndex = 1;
+		Intersection intersection = wa.getDeparture();
+		Tour t = w.graphicalView.getTour();
+		//changeOrder (Tour tour, Intersection intersection, int newIndex)
+		
 	}
 }
