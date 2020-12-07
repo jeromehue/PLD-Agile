e3e0654629de01f62f7fc5050edc2f88c2bf42ec
jerome <hue.jerome@yahoo.fr>
2020-12-07 10:48:24 +0100

    Solved a bug when canceleing order modification

--- src/main/java/controller/OrderModificationState.java
+++ src/main/java/controller/OrderModificationState.java
@@ -20,13 +20,14 @@ public class OrderModificationState implements State {
 	private static final Logger logger = LoggerFactory.getLogger(OrderModificationState.class);
 
 	public void clickOnStep(Controller c, Window w, Way wa, JButton button, Tour t) {
-		logger.info("[Order Modification State] Modify order of tour in controller");
-		logger.info("[Order Modification State] Intersection to be modify" + wa.getDeparture());
 		
-		// Testing purposes
-		int newIndex = w.displaySelectOrderDialog();
-		Intersection intersection = wa.getDeparture();
+		logger.info("Modify order of tour in controller");
+		
 
+		int newIndex = w.displaySelectOrderDialog();
+		if (newIndex != 0 ) {		
+		logger.info("New index : {}", newIndex);
+		Intersection intersection = wa.getDeparture();
 		Tour t1 = w.getGraphicalView().getTour(); 
 		CityMap cityMap = w.getGraphicalView().getCityMap();
 		Request request = w.getGraphicalView().getRequest();
@@ -38,5 +39,17 @@ public class OrderModificationState implements State {
 		t1.setTour(newTour);
 		t1.notifyObservers();
 		c.setCurrentstate(c.tourModificationState);
+		}
 	}
+	
+	@Override
+	public void modifyTour(Controller c, Window w) {
+		w.changeOptionalsButtonsVisibility();
+		if (w.isOptionalsButtonsVisible()) {
+			c.setCurrentstate(c.tourModificationState);
+		} else {
+			c.setCurrentstate(c.requestLoadedState);
+		}
+	}
+	
 }
