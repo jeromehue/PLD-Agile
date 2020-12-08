a83bc6ee1588629853e09e20edf6b58cee1ccfb4
sylvain <sylvain.de-joannis-de-verclos@insa-lyon.fr>
2020-12-08 15:39:35 +0100

    minor changes

--- src/main/java/view/Window.java
+++ src/main/java/view/Window.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridLayout;
 import java.io.File;
 import java.util.ArrayList;
 
