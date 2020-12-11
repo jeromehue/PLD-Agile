1ee4bd8682c391c8062223682c7c640807e3df7a
Charly Poirier <pro.poiriercharly@gmail.com>
2020-12-11 22:00:06 +0100

    Javadoc pour Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -11,8 +11,17 @@ import org.slf4j.LoggerFactory;
  * Main class, should be run to start the program
  */
 public class Main {
+
+	/**
+	 * The logger instance, used to log relevant information to the console.
+	 */
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+	/**
+	 * The main method, starting point of Deliver'IF. Creates a Controller instance.
+	 * 
+	 * @param args arguments (ignored)
+	 */
 	public static void main(String[] args) {
 		logger.info("Creating controller");
 		Locale.setDefault(new Locale("en", "US"));
